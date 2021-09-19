642ea8e20cd3e755da9ecba4ffc37d016c10435a
Abhishek <abhimore74@gmail.com>
2021-09-18 19:16:19 -0500

    browser

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ const MessagingResponse = require('twilio').twiml.MessagingResponse;
 const bodyParser = require('body-parser');
 const app = express();
 
+const ngrok = "http://a563-97-105-8-131.ngrok.io"
+
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.post('/', async (req, res) => {
@@ -25,16 +27,27 @@ app.post('/', async (req, res) => {
   const twilioPhoneNum = req.body.To
   var response = ""
 
+ //client.messages
+    //.create({body: "AAAAA", mediaUrl: ['https://tangerine-deer-7639.twil.io/assets/example.png'], from: twilioPhoneNum, to: senderPhoneNum})
+
   try {
     response = await utils.router(message);
   } catch(err) {
     response = "Unable to handle query! Type 'help' for help";
   }
 
-  client.messages
+  if(response.includes("http")) {
+    client.messages
+        .create({body: response, mediaUrl: [ngrok + '/images/example.png'], from: twilioPhoneNum, to: senderPhoneNum})
+  } else {
+    client.messages
         .create({body: response, from: twilioPhoneNum, to: senderPhoneNum})
+  }
+
 
 });
+
+app.use('/images', express.static(__dirname + '/images'));
 
 http.createServer(app).listen(1337, () => {
   console.log('Express server listening on port 1337');
--- utils.js
+++ utils.js
@@ -6,6 +6,7 @@
 
 const query = require('./routes/query.js')
 const maps = require('./routes/map.js')
+const browser = require('./routes/browser.js')
 
 function firstWord(str) {
   var words = str.trim().split(" ");
@@ -15,12 +16,19 @@ function firstWord(str) {
 async function router(message) {
   message = message.toLowerCase();
   let cmd = firstWord(message);
+  
+  if(message.includes("http://") || message.includes("https://")) {
+    cmd = 'browser';
+  }
 
   switch(cmd) {
     case "stellar":
       return "S"
     case "maps":
       var returnMessage = await maps.maps(message);
+      return returnMessage;
+    case "browser":
+      var returnMessage = await browser.browser(message);
       return returnMessage;
     default:
       var returnMessage = await query.query(message);
